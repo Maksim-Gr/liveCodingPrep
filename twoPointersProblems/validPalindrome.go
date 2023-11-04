dff1f8d239bab9f18c416759ef026919bd613c6c
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-11-04 20:42:46 +0300

    chore: add testCases

--- twoPointersProblems/validPalindrome.go
+++ twoPointersProblems/validPalindrome.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Check the valid palindrome using two pointers approach
 
 func IsValidPalindrome(inputString string) bool {
@@ -16,5 +18,10 @@ func IsValidPalindrome(inputString string) bool {
 }
 
 func main() {
+	str := []string{"ABC", "A", "ABC"}
+	for i, s := range str {
+		fmt.Printf("Test Case # %d\n", i+1)
+		fmt.Printf("\nIs it a palindrome?.....%v\n", IsValidPalindrome(s))
+	}
 
 }
