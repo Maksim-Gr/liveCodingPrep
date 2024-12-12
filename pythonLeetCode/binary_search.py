c51267eb424a990f581d3622c151eda21d5360a0
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-12-12 20:32:22 +0300

    recursive binary python

--- pythonLeetCode/binary_search.py
+++ pythonLeetCode/binary_search.py
@@ -18,7 +18,22 @@ class Solution:
         return -1
 
 
+    @staticmethod
+    def binary_search_recursive(nums: List[int], target: int,low:int, high:int) -> int:
+        if low > high:
+            return -1
+        mid  = int((low + high) / 2)
+        if nums[mid] == target:
+            return mid
+        elif target < nums[mid]:
+            return Solution.binary_search_recursive(nums, target, low, mid - 1)
+        else:
+            return Solution.binary_search_recursive(nums, target, mid + 1, high)
+
+
+
 sorted_nums = [-1,0,3,5,9,12]
 target = 9
 s = Solution()
 print(s.binary_search(sorted_nums, target))
+print(s.binary_search_recursive(sorted_nums, target, 0, len(sorted_nums) - 1))
