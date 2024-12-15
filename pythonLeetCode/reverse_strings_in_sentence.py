7347681e7d3ce5d255db78be8e9961fb3bc41d04
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-12-15 20:39:39 +0300

    str_rev helper function

--- pythonLeetCode/reverse_strings_in_sentence.py
+++ pythonLeetCode/reverse_strings_in_sentence.py
@@ -7,3 +7,12 @@ def reverse_words(sentence: str) -> str:
     sentence =  list(sentence)
     str_len = len(sentence) - 1
 
+
+
+
+
+def str_rev(_str, start, end):
+    while start < end:
+        if _str[start] != ' ':
+            break
+        start += 1
