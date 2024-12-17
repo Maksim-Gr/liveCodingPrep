35be14ed2f16aba47179ba9e70ae04ad4515877f
Makism-Gr <maksim.g.grishchenko@gmail.com>
2024-12-17 19:55:55 +0300

    reverse method for words in sentence

--- pythonLeetCode/reverse_strings_in_sentence.py
+++ pythonLeetCode/reverse_strings_in_sentence.py
@@ -7,12 +7,24 @@ def reverse_words(sentence: str) -> str:
     sentence =  list(sentence)
     str_len = len(sentence) - 1
 
+    str_rev(sentence, 0, str_len)
+    start = 0
+
+    for end in range(0, str_len + 1):
+        if end == str_len or sentence[end] == ' ':
+            end_idx = end if end == str_len else end - 1
+            str_rev(sentence, start, end_idx)
+            start = end + 1
+
+    return ''.join(sentence)
+
 
 
 
 
 def str_rev(_str, start, end):
     while start < end:
-        if _str[start] != ' ':
-            break
+        _str[start], _str[end] = _str[end], _str[start]
         start += 1
+        end -= 1
+
